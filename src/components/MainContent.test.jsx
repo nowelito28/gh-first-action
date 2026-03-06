14df269c80d10d1bb3ae30d9f05d08ee4a3dd91c
Nowel <208055845+nowelito28@users.noreply.github.com>
2026-03-06 18:04:45 +0100

    prove skipping workflows [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,6 +4,8 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 
+// Example for skip the ci/workflow
+
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
